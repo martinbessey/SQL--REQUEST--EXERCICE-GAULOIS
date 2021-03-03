b9784c3de9dd8c525ce4f1f103fa7dc5568a2fd6
Martin BESSEY <77329079+martinbessey@users.noreply.github.com>
2021-03-03 16:34:52 +0100

    Add files via upload

--- 7).sql
+++ 7).sql
@@ -1,4 +1,8 @@
-SELECT NOM, MAX(QTE)
-FROM villageois v, prise_casque p, bataille b
-WHERE b.NOM_BATAILLE='Babaorum'
-GROUP BY NOM
+SELECT v.NOM, b.NOM_BATAILLE, SUM(pc.QTE) AS NB_CASQUES
+FROM villageois v, bataille b, prise_casque pc
+WHERE pc.ID_VILLAGEOIS = v.ID_VILLAGEOIS
+AND pc.ID_BATAILLE = b.ID_BATAILLE
+GROUP BY v.NOM, b.NOM_BATAILLE
+HAVING NB_CASQUES = (SELECT MAX(sc.NB_CASQUES)
+                        FROM score_casques sc
+                        WHERE sc.NOM_BATAILLE = 'Babaorum')
